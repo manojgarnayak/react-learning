e89e03c39bb629c84294a44fa20b329601499f1d
manojgarnayak <manojgarnayak07@gmail.com>
2024-01-16 12:15:48 +0530

    File Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Chai from "./Chai"
 
 function App() {
-
+  const username = "Manoj"
   return (
     <>
       <Chai />
       <h1>Hii but i am a coffee lover</h1>
+      <h1>My name is {username}</h1>
     </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom App!</h1>
+        </div>
+    )
+} 
+
+const ReactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'click me to visit google'
+}
+
+const AnotherElement = (
+    <a href='http://google.com' target="_blank">Visit Google</a>
+)
+
+const anotherUser = "Bhalu"
+const reactElement = React.createElement(
+    'a',
+    {href: "https://google.com", target: '_blank'},
+    'Click me to visit this website',
+    anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <App/>
 )
